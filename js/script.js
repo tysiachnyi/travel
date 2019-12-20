80fb418a74e18f7e7160f7a9de91f028004fd903
Nikita <itomatpast@gmail.com>
2019-12-20 17:47:43 +0200

    making tab logick

---
+++ js/script.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', function(){
+'use strict';
+
+let tab = document.querySelectorAll('.info-header-tab'),
+    info = document.querySelector('.info-header'),
+    tabContent = document.querySelectorAll('.info-tabcontent');
+
+function hideTabContent(a){
+    for (let i = a;i < tabContent.length;i++){
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add('hide');
+
+    }
+}
+
+hideTabContent(1);
+
+function showTabContent(b){
+    if (tabContent[b].classList.contains('hide')){
+        tabContent[i].classList.remove('hide');
+        tabContent[i].classList.add('show');
+    }
+}
+
+
+});
