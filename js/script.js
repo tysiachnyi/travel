18ff915b4beb831ece782c5b8b137272c0a32268
Nikita <itomatpast@gmail.com>
2019-12-20 17:58:48 +0200

    add hide animation function to tab

--- js/script.js
+++ js/script.js
@@ -17,10 +17,23 @@ hideTabContent(1);
 
 function showTabContent(b){
     if (tabContent[b].classList.contains('hide')){
-        tabContent[i].classList.remove('hide');
-        tabContent[i].classList.add('show');
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
     }
 }
+
+info.addEventListener('click', function(event){
+    let target = event.target;
+    if(target && target.classList.contains('info-header-tab')){
+        for(let i = 0;i < tab.length;i++){
+            if(target == tab[i]){
+                hideTabContent(0);
+                showTabContent(i);
+                break;
+            }
+        }
+    }
+})
 
 
 });
