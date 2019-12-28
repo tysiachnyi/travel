74584aeff4a0e27df104f92ce9a652fb63c49cef
Nikita <itomatpast@gmail.com>
2019-12-28 14:42:23 +0200

    added slider and calculator

--- js/script.js
+++ js/script.js
@@ -144,16 +144,13 @@ window.addEventListener('DOMContentLoaded', function() {
                     request.open('POST', 'server.php');
                     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
                     request.onreadystatechange = function(){
-                        if(request.readyState < 4){
-                            resolve()
-                        }else if (request.readyState === 4){
-                            if(request.status == 200 && request.status < 3){
+                            if(request.status == 200 ){
                                 resolve()
                             }
                             else{
                                 reject()
                             }
-                        }
+                        
                     }
                     request.send(data);
                 })
@@ -166,77 +163,111 @@ window.addEventListener('DOMContentLoaded', function() {
                 }
             }
             postData(formData)
-                .then(()=> statusMessage.innerHTML = message.loading)
                 .then(()=>statusMessage.innerHTML = message.success)
                 .catch(()=>statusMessage.innerHTML = message.failure)
-                .then(clearInput)
+                .finally(clearInput)
 
         })
     }
 
     sendForm(form);
     sendForm(secondForm);
-    // form.addEventListener('submit', function(event){
+    
 
-    //     function sendForm(){
-    //         console.log('started promise fnc')
-    //         return new Promise(function(resolve, reject){
-               
-                
+    // slider
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
 
-               
-                
-                
+    showSlides(slideIndex);
+    
+    function showSlides(n) {
 
-               
+        if(n > slides.length){
+            slideIndex = 1;
+        }
+        if(n < 1){
+            slideIndex = slides.length;
+        }
 
-                
-    //         })
-    //     };
-
-    //     sendForm().then(()=>alert('nice'))
-    //               .catch(()=>alert('not nice'))
-
-
+        slides.forEach((item) => item.style.display = 'none');
+        dots.forEach((item) => item.classList.remove('dot-active'));
         
-       
-    // });
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n){
+
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n){
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function(){
+        plusSlides(-1);
+    });
+    next.addEventListener('click', function(){
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event){
+        for(let i=0; i < dots.length + 1; i++){
+            if(event.target.classList.contains('dot') && event.target == dots[i-1]){
+                currentSlide(i);
+            }
+        }
+    });
+
+    // calc
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.innerHTML = 0;
+
+    persons.addEventListener('change', function(){
+        personsSum =+ this.value;
+        total = (daysSum + personsSum)*4000;
+        console.log('personsSum ' + personsSum);
+        console.log('restDays.value ' + restDays.value);
+        if(restDays.value == '' || personsSum == 0 || persons.value == ''  || daysSum == 0){
+            totalValue.innerHTML = 0;
+        }else{
+            totalValue.innerHTML = total;
+        }
+    });
+
+    restDays.addEventListener('change', function(){
+        daysSum =+ this.value;
+        total = (daysSum + personsSum)*4000;
+        console.log('daysSum ' + daysSum);
+        console.log('persons.value ' + persons.value);
+        if(persons.value == ''  || daysSum == 0 || restDays.value == '' || personsSum == 0){
+            totalValue.innerHTML = 0;
+        }else{
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change', function(){
+        if(restDays.value == '' || persons.value == '' || persons.value == 0 || restDays == 0){
+            totalValue.innerHTML = 0;
+        }else{
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+        }
+    });
 
 
-
-//     secondForm.addEventListener('submit', function(event){
-//         event.preventDefault();
-//         secondForm.appendChild(statusMessage);
-        
-
-//         let request = new XMLHttpRequest();
-//         request.open('POST', 'server.php');
-//         request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-
-//         let formData = new FormData(secondForm);
-//         //  Convert FormData To JSON Object
-//         let obj = {};
-//         formData.forEach(function(value,key){
-//             obj[key] = value;
-//         });
-
-//         let json = JSON.stringify(obj);
-//         request.send(json);
-
-//         request.addEventListener('readystatechange', function(){
-//             if(request.readyState < 4){
-//                 statusMessage.innerHTML = message.loading;
-//             } else if(request.readyState === 4 && request.status == 200){
-//                 statusMessage.innerHTML = message.success;
-//             }else{ 
-//                 statusMessage.innerHTML = message.failure;
-//             }
-
-//             for (let i = 0;i<input.length;i++){
-//                 secondFormInput[i].value = ''
-//             };
-//         });
-
-//     });
 
 });
